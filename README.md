f2e1949ba38b1c3f2e08435dae9ca6e0b12c9f07
Paul Michalet <paul.michalet@gmail.com>
2012-10-20 12:41:19 +0200

    pushing v1.1

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-drupal-bash-setup
-=================
+drupal-quickup
+==============
 
-Usual Drupal installation
-
+Usual Drupal installation shell script
 Usage : cd to the folder that will be used to install Drupal (e.g. cd /var/www/mysite) and enter :
 
-$ wget "https://raw.github.com/Paulmicha/drupal-bash-setup/master/drupal_setup.sh" --quiet --no-check-certificate
-$ chmod +x "drupal_setup.sh"
+<pre>wget "https://raw.github.com/Paulmicha/drupal-quickup/master/drupal_setup.sh" --quiet --no-check-certificate
+chmod +x "drupal_setup.sh"</pre>
 
 Edit to your convenience and execute :
 
-$ ./drupal_setup.sh
+<pre>./drupal_setup.sh</pre>
--- drupal_setup.sh
+++ drupal_setup.sh
@@ -2,10 +2,13 @@
 # -*- coding: UTF8 -*-
 
 ##
-#   Simple Bash Drupal install with Drush for local dev
+#   Drupal simple Shell install script for rapid dev start
+#   Requires drush
 #
 #   @see http://drush.ws
 #   @see http://drushmake.me
+#
+#   @version 1.1
 #   @author Paulmicha
 #
 
@@ -61,6 +64,7 @@ drush si $DRUPAL_PROFILE --db-url=mysql://$DB_USERNAME:$DB_PASSWORD@$DB_HOST/$DB
 mkdir sites/all/libraries
 mkdir sites/all/modules/custom
 mkdir sites/all/modules/contrib
+#mkdir sites/all/modules/features
 chown $DEFAULT_UNIX_OWNER:$DEFAULT_UNIX_GROUP . -R
 chmod $DEFAULT_UNIX_MOD . -R
 
@@ -82,7 +86,7 @@ drush vset --yes file_temporary_path $DRUPAL_TMP_FOLDER
 #       From minimal install
 #       to my "standard" dev setup
 
-#       Minimal install : missing useful Drupal core modules
+#       Minimal install : missing needed Drupal core modules
 drush en taxonomy field_ui -y
 
 #       Drupal modules : minimum + dev + admin
@@ -90,7 +94,6 @@ drush dl admin_menu devel backup_migrate libraries transliteration
 drush en admin_menu admin_menu_toolbar devel backup_migrate libraries transliteration -y
 
 #       DB dump 1 : "standard" install restore point
-drush cc all
 drush bb
 
 
@@ -111,72 +114,171 @@ drush dl entity
 drush en entity -y
 drush dl entityreference
 drush en entityreference -y
-drush dl relation
-drush en relation relation_ui -y
+#       @see also http://drupal.org/project/entity_tree
+#drush dl relation
+#drush en relation relation_ui -y
 
 #       Media
 drush dl media-7.x-2.x-dev file_entity
 drush en file_entity media -y
+#drush dl bulk_media_upload
+#drush en bulk_media_upload -y
+#drush dl plupload
+#       @todo : get lib from https://github.com/downloads/moxiecode/plupload/plupload_1_5_4.zip
+#drush en plupload -y
+
+#       File storage
+#drush dl storage_api
+#drush en storage_api -y
 
 #       SEO
-drush dl pathauto redirect globalredirect
-drush en pathauto redirect globalredirect -y
-drush dl subpathauto
-drush en subpathauto -y
 drush dl page_title metatag
 drush en page_title metatag -y
-drush dl xmlsitemap
-drush en xmlsitemap -y
+drush dl pathauto redirect globalredirect
+drush en pathauto redirect globalredirect -y
+#drush dl subpathauto
+#drush en subpathauto -y
+#drush dl xmlsitemap
+#drush en xmlsitemap -y
 
 #       Linked Data
 drush dl microdata
 drush en microdata -y
 
+
+#-----------------------------------------
+#       HTML Emailing : swiftmailer
+
+cd sites/all/libraries
+
+#       check link for latest version
+#       @see http://swiftmailer.org/
+wget http://swiftmailer.org/download_file/Swift-4.2.1.tar.gz --quiet
+tar -zxf Swift-4.2.1.tar.gz
+mv Swift-4.2.1 swiftmailer
+rm Swift-4.2.1.tar.gz
+chown $DEFAULT_UNIX_OWNER:$DEFAULT_UNIX_GROUP . -R
+chmod $DEFAULT_UNIX_MOD . -R
+
+cd ../../../
+
+drush dl swiftmailer
+drush en swiftmailer -y
+
+
 #       DB dump 2 : "usual" install restore point
-drush cc all
 drush bb
 
 
 #-----------------------------------------
-#       "Opinionated" part
+#       UX / Redaction helpers
 
-#       A Drupal base theme that rocks
-drush dl mothership
-drush en mothership -y
+#drush dl token_filter
+#drush en token_filter -y
+
+
+#-----------------------------------------
+#       Workflow & content moderation
+
+#drush dl revisioning
+#drush en revisioning -y
+
+#drush dl wokflow
+#drush en wokflow workflow_admin_ui workflow_access -y
+
+
+#-----------------------------------------
+#       Site building / Dev utils
+
+#drush dl features
+#drush en features -y
+
+#       DB-related utils
+#drush dl schema
+#drush en schema -y
+
+#       Drush extensions
+#drush dl drush_iq
+#drush en drush_iq -y
+
+
+#-----------------------------------------
+#       "Opinionated" part (front-end)
+
+#       Base theme
+#drush dl mothership
+#drush en mothership -y
+#drush dl omega
+#drush en omega -y
 
 #       Layout management
+#drush dl ds
+#drush en ds -y
+#drush dl themekey
+#drush en themekey -y
 #drush dl context
 #drush en context -y
 #drush dl delta
 #drush en delta -y
-#drush dl ds
-#drush en ds -y
 
-#       DB dump 3 : "start" restore point
-drush cc all
-drush bb
+#       Front-end app architecture
+#drush dl backbone
+#drush en backbone -y
+
+#       Breadcrumbs
+#drush dl crumbs
+#drush en crumbs -y
+#drush dl path_breadcrumbs
+#drush en path_breadcrumbs -y
+
+#       CSS / Styling
+drush dl styleguide
+drush en styleguide -y
 
 
 #-----------------------------------------
 #       Performance
+
+#drush dl js
+#drush en js -y
+
+#drush dl js_callback
+#drush en js_callback -y
+
+drush dl fast_404
+drush en fast_404 -y
 
 #drush dl entitycache
 #drush en entitycache -y
 
 #drush dl filecache
 #drush en filecache -y
+#       OR
+#drush dl boost
+#drush en boost -y
 
-#drush dl varnish
-#drush en varnish -y
-
-#drush dl memcache
-#drush en memcache -y
+#drush dl cdn
+#drush en cdn -y
 
 #drush dl apc
 #drush en apc -y
 
-#drush dl boost
-#drush en boost -y
+#drush dl memcache
+#drush en memcache -y
 
+#drush dl varnish
+#drush en varnish -y
+
+#drush dl esi
+#drush en esi -y
+
+#       Cron
+#drush dl elysia_cron
+#drush en elysia_cron -y
+
+
+
+#       DB dump 3 : "start" restore point
+drush bb
 
 
